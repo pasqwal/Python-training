3cac573ea17be2282a747e52c406179a3b6168fc
pasqwal <courty.pascal@gmail.com>
2017-02-24 13:00:54 +0100

    add assets directory

--- db/manage_db.py
+++ db/manage_db.py
@@ -1,12 +1,16 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+"""
+This Module allows easy db management of sqlite
+----------------------------------------
+
+
+"""
 
 import sqlite3 as lite
 
-# SQL_CREATE = "CREATE TABLE IF NOT EXISTS sncf (date, nom_train, ponctualite)"
 SQL_CREATE = "CREATE TABLE IF NOT EXISTS {table} ({fields}) "
-# SQL_INSERT = "INSERT INTO sncf values(?, ?, ?)"
 SQL_INSERT = "INSERT INTO {table} values({values})"
 
 class CreateDb:
@@ -16,18 +20,25 @@ class CreateDb:
         self._connection = lite.connect(path + self._name)
         self.cursor = self._connection.cursor()
 
-    def createTable(self, tablename='test', liste_champs=('champ_test')):
+    def createTable(self, tablename='test', liste_champs=['champ_test']):
+        print(SQL_CREATE.format(table=tablename, fields=liste_champs))
         self.cursor.execute(SQL_CREATE.format(table=tablename, fields=liste_champs))
 
-    def insertIntoTable(self, tablename='test', values=('record1',)):
-        format_nb_champs = ','.join('?'*len(values))
+    def insertIntoTable(self, tablename='test', values=[('record1',)]):
+        format_nb_champs = ','.join('?'*len(values[0]))
         print(SQL_INSERT.format(table=tablename, values=format_nb_champs))
         self.cursor.executemany(SQL_INSERT.format(table=tablename, values=format_nb_champs), values)
 
     def getDataFromTable(self, tablename='test'):
         self.cursor.execute("SELECT * FROM {}".format(tablename))
         rows = self.cursor.fetchall()
-        return rows
+        return rows  # return a tuple sequence
+
+    def printDataFromTable(self, tablename='test'):
+        for row in db.getDataFromTable(tablename):
+            print([field for field in row])
+
+
 
     def close(self):
         self._connection.close()
@@ -37,8 +48,17 @@ if __name__ == "__main__":
         db = CreateDb()
         db.createTable()
         db.insertIntoTable()
-        rows = db.getDataFromTable()
-        for row in rows:
-            print(row)
+        records =[]
+        records.append(('record2',))
+        records.append(('record3',))
+        db.insertIntoTable('test', records)
+
+        db.printDataFromTable()
+
+        db.createTable('client',('nom', 'prenom'))
+        records = []
+        records.append(('Jon','Doe'))
+        records.append(('Jane', 'Jane'))
+        db.insertIntoTable('client', records)
     finally:
         db.close()
